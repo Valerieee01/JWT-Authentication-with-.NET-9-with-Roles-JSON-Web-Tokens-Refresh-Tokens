a6c922a3f54b8b0dc022167aa0cc2fde92108951
Valerieee01 <zharmel.afa@gmail.com>
2025-07-29 15:43:25 -0500

    Primera parte JWT apena se creo register y login sin implementar

---
+++ JwtAuthDotNet9/Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using JwtAuthDotNet9.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using JwtAuthDotNet9.Entities;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+
+namespace JwtAuthDotNet9.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] 
+    public class AuthController : ControllerBase
+    {
+
+        public static User user = new();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDTOs request)
+        {
+            var hashesdPassword = new PasswordHasher<User>()
+                .HashPassword(user, request.Password);
+
+            user.Username = request.Username;
+            user.PasswordHash = hashesdPassword;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<String> Login(UserDTOs request)
+        {
+            if (user.Username != request.Username)
+            {
+                return BadRequest("User not Found");
+            }
+
+            if (new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password) 
+                == PasswordVerificationResult.Failed)
+            {
+                return BadRequest("Wrong Password.");
+            }
+
+            string token = "suscces";
+
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key  = new SymmetricSecurityKey()
+        }
+    }
+}
---
+++ JwtAuthDotNet9/Models/UserDTOs.cs
@@ -0,0 +1,9 @@
+﻿namespace JwtAuthDotNet9.Models
+{
+    public class UserDTOs
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
